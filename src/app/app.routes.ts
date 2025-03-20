86478ea264739d6934c9cf4526c93a1c5eaad64d
Javor Emir <emir.j2002@gmail.com>
2025-03-20 22:37:36 +0100

    init dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './features/landing/landing.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: LandingComponent,
-        pathMatch: 'full'
-    },
-    {
-        path: '**',
-        redirectTo: ''
-    }
+  {
+    path: '',
+    component: LandingComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
